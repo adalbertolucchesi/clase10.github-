4863f9a06c0082975e5bbec36399826160402ca9
adalbertolucchesi <130623675+adalbertolucchesi@users.noreply.github.com>
2023-05-21 20:01:51 -0300

    Add files via upload

---
+++ clase10.js
@@ -0,0 +1,19 @@
+const nombre = "adalberto";
+const apellido = "lucchesi";
+console.log(nombre +" "+ apellido);
+
+
+
+const num1 = 5;
+const num2 = 10;
+const suma = num1 + num2;
+console.log("resultado parcial", suma);
+
+const num3 = 3;
+const resultadoTotal = suma + num3;
+console.log( "resultado final", resultadoTotal);
+
+     
+  
+  
+
